2af1051084286a3d190bb24fa6c7877d8409a271
Mateus Guerra <mateus.guerra@nateevo.com>
2020-12-23 16:38:17 +0100

    animation and gif generator

--- anima-seq-v/js/index.js
+++ anima-seq-v/js/index.js
@@ -2,11 +2,20 @@
 
 const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
-const snap = document.getElementById("snap");
+const canvasGetImage = document.getElementById('canvas-get-image');
+const canvasFrame = document.getElementById('canvas-frame');
+const canvasCH001 = document.getElementById('canvas-ch001');
+const canvasCH002 = document.getElementById('canvas-ch002');
+// const snap = document.getElementById("snap");
 const errorMsgElement = document.querySelector('span#errorMsg');
+let imgObj = {
+  imgArray001: [],
+  imgArray002: []
+}
+let imgArray = [];
 
 const constraints = {
-  audio: true,
+  audio: false,
   video: {
     width: 640, height: 480
   }
@@ -37,15 +46,83 @@ init();
 //         context.drawImage(video, 0, 0, 640, 480);
 // });
 
-// store image 
 
-var context = canvas.getContext('2d');
-document.addEventListener('keydown', (event) => {
-const keyName = event.code;
+// Save as gif
+var gif = new GIF({
+  workers: 2,
+  quality: 10
+});
 
-if(keyName == "Space"){
-    context.drawImage(video, 0, 0, 640, 480);
-    var img = canvas.toDataURL('image/jpeg');
+
+let generateGif = function(imgs){
+  // add an image element
+  var ctxGetImage= canvasGetImage.getContext('2d');
+  for(var i = 0; i < imgs.length; i++ ){
+    
+    var image = new Image();
+    image.onload = function() {
+      ctxGetImage.drawImage(image, 0, 0);
+    };
+
+    if(imgs[i] != undefined){
+      image.src = imgs[i];
+      // console.log(ctxGetImage);
+      
+      gif.addFrame(image);
+    }
+    
+  }
+
+  gif.on('finished', function(blob) {
+    window.open(URL.createObjectURL(blob));
+  });
+  
+  gif.render();
 }
 
+var cGrabFrame = canvasFrame.getContext('2d');
+document.addEventListener('keydown', (event) => {
+  const keyName = event.code;
+  console.log(keyName);
+  
+  // grab image "Space"
+  if(keyName == "Space"){
+    cGrabFrame.drawImage(video, 0, 0, 640, 480)
+    imgArray.push(canvasFrame.toDataURL('image/jpeg').replace("image/jpeg", "image/octet-stream"));
+  }
+  //  clear images "c"
+  if(keyName == "KeyC"){
+    imgArray = [];
+  }
+
+  // Onion skin "o"
+  if(keyName == "KeyO"){
+    document.getElementById('video').toggleAttribute("hidden");
+  }
+  
+  // generate GIF "s"
+  if(keyName == "KeyS" && imgArray.length > 0){
+    generateGif(imgArray);
+  }
+
 });
+
+var canvasPlay = canvas.getContext('2d');
+//var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+
+
+var index = 0; 
+setInterval(function(){ 
+  if(imgArray.length > 0){
+    index == imgArray.length ? index = 0 : index++
+    var image = new Image();
+    image.onload = function() {
+      canvasPlay.drawImage(image, 0, 0);
+    };
+    if(imgArray[index] != undefined){
+      image.src = imgArray[index];
+    }
+  }
+ }, 300);
+
+
